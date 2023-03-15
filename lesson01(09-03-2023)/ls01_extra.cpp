4097682a32eafbf4a56830e873ee6faade57a896
Danila Starikov <1132226531@pfur.ru>
2023-03-15 21:33:18 +0300

    feat(main): finished ls01_extra

--- lesson01(09-03-2023)/ls01_extra.cpp
+++ lesson01(09-03-2023)/ls01_extra.cpp
@@ -16,6 +16,11 @@ protected:
     */
     unsigned short size;
     unsigned short age;
+
+    void print_name() {
+        cout << this->name << " ";
+    }
+
 public:
     Animal(string name, string class_is, string food, string sound, string color, unsigned short size, unsigned short age) {
         this->name = name;
@@ -26,9 +31,6 @@ public:
         this->size = size;
         this->age = age;
     }
-    void print_name() {
-        cout << this->name << " ";
-    }
     void breathe() {
         print_name();
         cout << "inhales... exhales..." << endl;
@@ -37,9 +39,12 @@ public:
         print_name();
         cout << "eats " << this->food << endl;
     }
-    void eat(string unusual_food) {
-        print_name();
-        cout << "eats " << unusual_food << " . It's bad." << endl;
+    void eat(string food) {
+        if (food != this->food) {
+            print_name();
+            cout << "eats " << food << ". It's bad." << endl;
+        }
+        eat();
     }
     void eat(short portion_count) {
         print_name();
@@ -58,7 +63,46 @@ public:
 
 class Mamal: public Animal {
 protected:
-    ;
+    bool fur;
+    unsigned short fur_length; // from 0 to 10, 10 - max
+public:
+    Mamal(string name, string class_is, string food, string sound, string color, unsigned short size, unsigned short age, bool fur, unsigned short fur_length): Animal(name, class_is, food, sound, color, size, age) {
+        this->fur = fur;
+        this->fur_length = fur_length;
+    }
+    void drop_fur() {
+        print_name();
+        cout << "drops fur." << endl;
+    }
+    void run() {
+        print_name();
+        cout << "runs" << endl;
+    }
+    void run(unsigned short distance) {
+        print_name();
+        cout << "runs " << distance << " m." << endl;
+    }
+    ~Mamal() {}
+};
+
+class Fish: public Animal {
+protected:
+public:
+    Fish(string name, string class_is, string food, string sound, string color, unsigned short size, unsigned short age): Animal(name, class_is, food, sound, color, size, age) {};
+    void swim() {
+        print_name();
+        cout << "swims" << endl;
+    }
+    void swim(short distance) {
+        print_name();
+        cout << "swam " << distance << " m." << endl;
+    }
+    void lay_eggs() {
+        print_name();
+        cout << "layed eggs." << endl;
+    }
+
+    ~Fish() {}
 };
 
 class Bird: public Animal {
@@ -68,30 +112,23 @@ public:
     Bird(string name, string class_is, string food, string sound, string color, unsigned short size, unsigned short age, unsigned short wings_size): Animal(name, class_is, food, sound, color, size, age) {
         this->wings_size = wings_size;
     }
-    ~Bird() {
-        print_name();
-        cout << "passed away." << endl;
-    }
     void fly() {
         print_name();
-        cout << "flies" << endl;
+        cout << "flew" << endl;
     }
     void fly(short distance) {
         print_name();
-        cout << "flied " << distance << "m." << endl;
+        cout << "flew " << distance << " m." << endl;
     }
-    ~Bird() {
-        print_name();
-        cout << "passed away." << endl;
-    }
+    ~Bird() {}
 };
 
 class Penguin: public Bird {
 protected:
 public:
-    Penguin(string name, string class_is, string food, string sound, string color, unsigned short size, unsigned short age, unsigned short wings_size) : Bird(name, class_is, food, sound, color, size, age, wings_size) {
+    Penguin(string name, string class_is, string food, string sound, string color, unsigned short size, unsigned short age, unsigned short wings_size): Bird(name, class_is, food, sound, color, size, age, wings_size) {
 
-    } 
+    }
     void fly() {
         print_name();
         cout << "cannot fly" << endl;
@@ -104,12 +141,25 @@ public:
         print_name();
         cout << "swam " << distance << " m." << endl;
     }
-    ~Penguin(){
-        print_name();
-        cout << "passed away" << endl;
-    }
+    ~Penguin() {}
 };
 
 int main() {
-    return 0;
+    Bird* crow = new Bird("Blanche", "bird", "dead meat", "Kaarrr", "black", 1, 7, 5);
+    Penguin* penguin = new Penguin("Alex", "bird", "fish", "squeak", "white", 1, 4, 3);
+    Fish* fish = new Fish("Nemo", "fish", "Plankton", "bubble pop", "silver", 4, 1);
+    crow->fly();
+    crow->fly(400);
+    crow->eat("dead meat");
+    penguin->fly();
+    penguin->swim(100);
+    penguin->eat(10);
+    fish->make_sound();
+    fish->swim(4000);
+    fish->lay_eggs();
+    fish->eat("Plankton");
+    delete penguin;
+    delete crow;
+    delete fish;
+        return 0;
 }
